bfbb6ed989818e28b36dd8e39fb9790ab845126f
11vanshika <vanshikaa@moback.com>
2022-09-30 10:30:54 +0530

    first commit

---
+++ Hospital_Oops/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Oops
+{
+    public class Hp
+    {
+        private int Id;
+        private int totalNumberOfDoctor;
+        private int totalNumberOfNurse;
+        private int totalNumberOfemployee;
+
+        public int id
+        {
+            set { Id = value; }
+            get { return Id; }
+        }
+       public int TotalNumberOfDoctor
+        {
+            set { totalNumberOfDoctor = value; }
+            get { return totalNumberOfDoctor; }
+        }
+        public int TotalNumberOfNurse
+        {
+            get { return totalNumberOfNurse; }
+            set { totalNumberOfNurse = value; }
+        }
+        public int TotalNumberOfEmployee
+        {
+            get { return totalNumberOfemployee; }
+            set { totalNumberOfemployee = value; }
+        }
+       
+        public int TotalNumber()
+        {
+         int sum =  TotalNumberOfDoctor + TotalNumberOfNurse + TotalNumberOfEmployee;
+            return sum;
+        }
+    }
+}
---
+++ Hospital_Oops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Oops
+{
+    class Class1{
+    static void Main(string[] args)
+    {
+        Hp hospital = new Hp();
+            hospital.id = 12;
+            hospital.TotalNumberOfDoctor = 23;
+            hospital.TotalNumberOfNurse = 13;
+            hospital.TotalNumberOfEmployee = 10;
+          int result =  hospital.TotalNumber();
+            Console.WriteLine("Total Number of Employee" + result);
+
+
+
+    }
+  }
+}
